0da29a5d1dcf3a6b552d8b6fa254c5f471f22e49
Brayden Houston <braydenjhouston@gmail.com>
2022-09-15 22:15:01 -0600

    Add some methods

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = Vue.createApp({
         return {
             goatCount: 5
         }
+    },
+    methods: {
+        addGoat(){
+            this.goatCount += 1
+        },
+        addFiveGoats(){
+            this.goatCount += 5
+        }
     }
 })
+
+
 
 const appInstance = app.mount('#app')
