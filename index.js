0fb30bb4bd84d2eca3fd0972fd1d42ee0a164e71
Brayden Houston <braydenjhouston@gmail.com>
2022-09-15 22:43:24 -0600

    list some goats

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const app = Vue.createApp({
     data() {
         return {
-            goatCount: 5
+            goats: [
+                {
+                    name: "Gruff",
+                    isGrumpy: true,
+                    powerLevel: 100
+                },
+                {
+                    name: "Billy",
+                    isGrumpy: false,
+                    powerLevel: 30
+                },
+                {
+                    name: "Joe",
+                    isGrumpy: false,
+                    powerLevel: 42
+                },
+                {
+                    name: "Sam",
+                    isGrumpy: true,
+                    powerLevel: 42
+                },
+                {
+                    name: "Goku",
+                    isGrumpy: true,
+                    powerLevel: 9001
+                }
+            ]
         }
     },
     computed: {
+        goatCount() {
+            return this.goats.length
+        },
         dailyFoodRequirments() {
             return this.goatCount * 5
         }
